5c8dd1b359c5a8a40253c3fdf6d488c68e322746
Tedd Arcuri <tarcuri88@gmail.com>
2014-05-24 12:55:58 -0600

    Built the nav

--- index.php
+++ index.php
@@ -9,8 +9,22 @@
 
 		<!-- Navigation -->
 		<nav>
+			<ul class="navigation">
+				<a class="logo"></a>
+				<li><a href="">Me, The Person</a></li>
+				<li><a href="">My Work</a></li>
+				<li><a href="">(Some Of) My Thoughts</a></li>
+				<li><a href="">Contact Me</a></li>
+			</ul>
 
+			<ul class="social">
+				<li><img src="img/github.svg" alt="" /></li>
+				<li><img src="img/github.svg" alt="" /></li>
+				<li><img src="img/github.svg" alt="" /></li>
+			</ul>
 		</nav>
+
+		<!-- Hello, My Name Is -->
 
 	</body>
 </html>
