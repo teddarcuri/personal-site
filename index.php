36711e456c23ca073c21ee645d730cbe67786ada
Tedd Arcuri <tarcuri88@gmail.com>
2014-05-24 12:05:26 -0600

    Initial commit, basics static pages

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	<head>
+		<title>Tedd Arcuri | Web Designer &amp; Developer</title>
+
+		<!-- Stylesheet -->
+		<link rel="stylesheet" href="/css/style.css" type="text/css" charset="utf-8">
+	</head>
+	<body>
+
+		<!-- Navigation -->
+		<nav>
+
+		</nav>
+
+	</body>
+</html>
